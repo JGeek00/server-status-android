e5af3cbf5788143a624a9a5c7978defd80a74a1b
Juan Gilsanz Polo <juangilsanz@gmail.com>
2024-11-02 03:51:24 +0100

    Fixed allow http connections

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.jgeek00.ServerStatus"
         minSdk = 30
         targetSdk = 35
-        versionCode = 2
-        versionName = "0.1.1"
+        versionCode = 5
+        versionName = "0.1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
