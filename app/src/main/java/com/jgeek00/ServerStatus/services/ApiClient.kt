c431bd34d9140ab4084c35bb0d56231e718a659e
Juan Gilsanz Polo <juangilsanz@gmail.com>
2024-11-03 19:45:19 +0100

    Use okhttp3

--- app/src/main/java/com/jgeek00/ServerStatus/services/ApiClient.kt
+++ app/src/main/java/com/jgeek00/ServerStatus/services/ApiClient.kt
@@ -5,41 +5,30 @@ import com.google.gson.JsonParser
 import com.jgeek00.ServerStatus.models.ServerModel
 import com.jgeek00.ServerStatus.models.StatusResult
 import com.jgeek00.ServerStatus.utils.createServerAddress
+import com.jgeek00.ServerStatus.utils.encodeCredentialsBasicAuth
 import com.jgeek00.ServerStatus.utils.transformStatusJSON
-import io.ktor.client.HttpClient
-import io.ktor.client.call.body
-import io.ktor.client.engine.android.Android
-import io.ktor.client.plugins.auth.Auth
-import io.ktor.client.plugins.auth.providers.basic
-import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
-import io.ktor.client.request.get
-import io.ktor.client.request.url
-import io.ktor.http.ContentType
-import io.ktor.http.contentType
-import io.ktor.http.isSuccess
-import io.ktor.serialization.gson.gson
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import okhttp3.OkHttpClient
+import okhttp3.Request
 
 class ApiClient {
-    private var client: HttpClient? = null
+    private var client: OkHttpClient? = null
+
     private var serverAddress: String? = null
 
     fun setApiClientInstance(server: ServerModel) {
-        client = HttpClient(Android) {
-            install(ContentNegotiation) {
-                gson {
-                    // Configure Gson if needed
+        client = if (server.useBasicAuth && server.basicAuthUser != null && server.basicAuthPassword != null) {
+            OkHttpClient.Builder()
+                .addInterceptor { chain ->
+                    val request = chain.request().newBuilder()
+                        .addHeader("Authorization", "Basic ${encodeCredentialsBasicAuth(server.basicAuthUser, server.basicAuthPassword)}")
+                        .build()
+                    chain.proceed(request)
                 }
-            }
-            if (server.useBasicAuth && server.basicAuthUser != null && server.basicAuthPassword != null) {
-                install(Auth) {
-                    basic {
-                        username = server.basicAuthUser
-                        password = server.basicAuthPassword
-                    }
-                }
-            }
+                .build()
+        } else {
+            OkHttpClient()
         }
 
         this.serverAddress = "${
@@ -57,17 +46,15 @@ class ApiClient {
 
         return withContext(Dispatchers.IO) {
             try {
-                val response = client!!.get {
-                    url("${serverAddress!!}/status")
-                    contentType(ContentType.Application.Json)
-                }
+                val request = Request.Builder()
+                    .url("${serverAddress!!}/status")
+                    .build()
 
-                if (response.status.isSuccess()) {
-                    val body = response.body<String>()
+                client!!.newCall(request).execute().use { res ->
+                    if (!res.isSuccessful || res.body == null) return@withContext null
+                    val body = res.body!!.string()
                     val formatted = transformStatusJSON(JsonParser.parseString(body))
                     return@withContext Gson().fromJson(formatted, StatusResult::class.java)
-                } else {
-                    return@withContext null
                 }
             } catch (e: Exception) {
                 println(e.localizedMessage)
