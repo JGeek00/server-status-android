4e2071bd8c9cb183139cec5e5171c3d5fc2c7fea
Juan Gilsanz Polo <juangilsanz@gmail.com>
2026-07-08 21:08:52 +0200

    Updated app version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.jgeek00.ServerStatus"
         minSdk = 30
         targetSdk = 37
-        versionCode = 19
-        versionName = "1.2.2"
+        versionCode = 20
+        versionName = "1.2.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
